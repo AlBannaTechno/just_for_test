27038ea6cc602927189daf68e1fe88d86a7f466f
Al_Banna_Techno <Al_Banna_Techno@yahoo.com>
2017-06-07 17:45:39 +0200

    add call fucntion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>;
+#include<GNS.h>
+#define __Allow__NUM_STR_CONV__x86__
 using namespace std;
+int get_call(char *);
 int main(){
 	int call;
 	char *name;
 	
+}
+int get_call(char *num){
+	return __NUMSTR(num,"x86");
 }
