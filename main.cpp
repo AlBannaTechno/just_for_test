57dcb8a4f29d1ad77d8eb2c50acac8c7312c6827
Al_Banna_Techno <Al_Banna_Techno@yahoo.com>
2017-06-07 17:48:59 +0200

    calling

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 #define __Allow__NUM_STR_CONV__x86__
 using namespace std;
 int get_call(char *);
-int main(){
+int main(int argc,char **argv){
 	int call;
 	char *name;
+	if(argc <2){
+		exit(0)
+	}
+	else{
+		__std__g_c_f_0xxff_(get_call(num),call);
+	}
 	
 }
 int get_call(char *num){
